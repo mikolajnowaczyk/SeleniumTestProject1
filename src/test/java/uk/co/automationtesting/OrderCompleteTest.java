c26b4d3d78c43e659b29dffdf7bf17478e4426ea
mikolajnowaczyk <mikolaj35@wp.pl>
2023-03-23 13:00:39 +0100

    Enrich test cases with additional logs and passes info

--- src/test/java/uk/co/automationtesting/OrderCompleteTest.java
+++ src/test/java/uk/co/automationtesting/OrderCompleteTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import base.ExtentManager;
 import base.Hooks;
 import pageObjects.HomePage;
 import pageObjects.OrderFormDelivery;
@@ -25,32 +26,37 @@ public class OrderCompleteTest extends Hooks {
 
 	@Test
 	public void endToEndTest() throws InterruptedException, IOException {
+		ExtentManager.log("Starting OrderCompleteTest...");
 		HomePage home = new HomePage();
 		home.getCookie().click();
 		home.getTestStoreLink().click();
+		ExtentManager.pass("Have successfully reached store homepage");
 
 		ShopHomepage shopHome = new ShopHomepage();
 		shopHome.getProdOne().click();
+		ExtentManager.pass("Have successfully reached shop product page");
 
 		ShopProductPage shopProd = new ShopProductPage();
 		Select option = new Select(shopProd.getSizeOption());
 		option.selectByVisibleText("M");
-
+		ExtentManager.pass("Have successfully selected product size");
 		shopProd.getQuantIncrease().click();
+		ExtentManager.pass("Have successfully increased quantity");
 		shopProd.getAddToCartBtn().click();
-
-		Thread.sleep(1500);
+		ExtentManager.pass("Have successfully added item to cart");
 
 		ShopContentPanel cPanel = new ShopContentPanel();
 		cPanel.getCheckoutBtn().click();
 
 		ShoppingCart cart = new ShoppingCart();
+		ExtentManager.pass("Have successfully reached the shoppign cart page");
 		cart.getHavePromo().click();
+		ExtentManager.pass("Have successfully selected the promo button");
 		cart.getPromoAddBtn().click();
 		cart.getPromoTextbox().sendKeys("20OFF");
 		cart.getPromoAddBtn().click();
-		Thread.sleep(1500);
 		cart.getProceedCheckoutBtn().click();
+		ExtentManager.pass("Have successfully selected the check out button");
 
 		OrderFormPersInfo pInfo = new OrderFormPersInfo();
 		pInfo.getGenderMr().click();
@@ -58,8 +64,8 @@ public class OrderCompleteTest extends Hooks {
 		pInfo.getLastnameField().sendKeys("Smith");
 		pInfo.getEmailField().sendKeys("johnsmith@test.com");
 		pInfo.getTermsConditionsCheckbox().click();
-		Thread.sleep(1500);
 		pInfo.getContinueBtn().click();
+		ExtentManager.pass("Have successfully entered customer details");
 
 		OrderFormDelivery orderDelivery = new OrderFormDelivery();
 		orderDelivery.getAddressField().sendKeys("123 Main Street");
@@ -67,16 +73,19 @@ public class OrderCompleteTest extends Hooks {
 		Select state = new Select(orderDelivery.getStateDropdown());
 		state.selectByVisibleText("Texas");
 		orderDelivery.getPostcodeField().sendKeys("77021");
-		Thread.sleep(1500);
 		orderDelivery.getContinueBtn().click();
+		ExtentManager.pass("Have successfully entered delivery informations");
 
 		OrderFormShippingMethod shipMethod = new OrderFormShippingMethod();
 		shipMethod.getDeliveryMsgTextbox().sendKeys("If I am no in, please leave my delivery on my porch.");
 		shipMethod.getContinueBtn().click();
+		ExtentManager.pass("Have successfully selected shipping method");
 
 		OrderFormPayment orderPay = new OrderFormPayment();
 		orderPay.getPayByCheckRadioBtn().click();
 		orderPay.getTermsConditionsCheckbox().click();
 		orderPay.getOrderBtn().click();
+		ExtentManager.pass("Have successfully placed order");
+
 	}
 }
