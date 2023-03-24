cfcdab60f6a2819a8d404f5ccb0dc2fc01a66fb5
mikolajnowaczyk <mikolaj35@wp.pl>
2023-03-24 10:02:55 +0100

    Add tests for login with data provided from excel sheet

---
+++ src/main/java/pageObjects/ShopLoginPage.java
@@ -0,0 +1,36 @@
+package pageObjects;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import base.BasePage;
+
+public class ShopLoginPage extends BasePage {
+	public WebDriver driver;
+
+	By email = By.cssSelector("section input[name='email']");
+	By password = By.cssSelector("input[name='password']");
+	By submitButton = By.cssSelector("button#submit-login");
+
+	public ShopLoginPage() throws IOException {
+		super();
+	}
+
+	public WebElement getEmail() throws IOException {
+		this.driver = getDriver();
+		return driver.findElement(email);
+	}
+
+	public WebElement getPassword() throws IOException {
+		this.driver = getDriver();
+		return driver.findElement(password);
+	}
+
+	public WebElement getSubmitButton() throws IOException {
+		this.driver = getDriver();
+		return driver.findElement(submitButton);
+	}
+}
